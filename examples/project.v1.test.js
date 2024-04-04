832505b89dd017944c877e3d48bfd7545a536fa6
dvesperini <dvesperini@gmail.com>
2024-04-04 16:28:19 +0200

    fix: test creation of new release (#13)
    Signed-off-by: dvesperini <dvesperini@gmail.com>

--- examples/project.v1.test.js
+++ examples/project.v1.test.js
@@ -87,7 +87,7 @@ describe('ProjectV1', () => {
 
     // ProjectPrototypeDefinition
     const projectPrototypeDefinitionModel = {
-      name: 'acme-microservice',
+      name: 'acme-microservice1',
       description: 'A microservice to deploy on top of ACME infrastructure.',
     };
 
