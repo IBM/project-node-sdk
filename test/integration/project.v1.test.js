e541afe9508d7921e3a494e527b24c0109386c55
dvesperini <dvesperini@gmail.com>
2024-04-18 11:44:41 +0200

    feat: test damiano
    Signed-off-by: dvesperini <dvesperini@gmail.com>

--- test/integration/project.v1.test.js
+++ test/integration/project.v1.test.js
@@ -55,7 +55,7 @@ describe('ProjectV1_integration', () => {
 
     // ProjectPrototypeDefinition
     const projectPrototypeDefinitionModel = {
-      name: 'acme-microservice1',
+      name: 'acme-microservice6',
       destroy_on_delete: true,
       description: 'A microservice to deploy on top of ACME infrastructure.',
       monitoring_enabled: false,
